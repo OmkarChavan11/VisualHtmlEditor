ec811a507fbb025fa21d9c8ec31e316a60206023
OmkarChavan11 <159440421+OmkarChavan11@users.noreply.github.com>
2024-02-28 16:47:55 +0530

    Add files via upload

---
+++ VisualHtmlEditor/html-editor/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HTMLVisualEditor from './components/HTMLVisualEditor'; // Adjust the path as needed
+
+function App() {
+  return (
+    <div className="App">
+      
+      <main>
+        <HTMLVisualEditor />
+      </main>
+    </div>
+  );
+}
+
+export default App;
